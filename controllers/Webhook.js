6471166b53b4aff74d7985ffb3492a2379219de8
shaunakk <epicshaunak@gmail.com>
2018-04-29 05:52:52 -0700

    paypal

--- controllers/Webhook.js
+++ controllers/Webhook.js
@@ -1,4 +1,16 @@
+const fs = require("fs");
 module.exports = (req, res) => {
-    console.log(req.body.resource.amount.total);
+    const transactionAmount = Number(req.body.resource.amount.total);
+    const fee = transactionAmount * 0.005;
+    let contents = String(fs.readFileSync('../data.json'));
+    contents = JSON.parse(contents);
+    contents.transactions.push({
+        "time": req.body.create_time,
+        fee,
+        transactionAmount
+    });
+    fs.writeFileSync('../data.json', JSON.stringify(contents));
+
+
     res.json({})
 }
